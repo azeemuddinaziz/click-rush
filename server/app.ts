0a1d80100582da780bd0060bd47cf4be615c0934
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2026-08-15 08:12:12 +0530

    backend: init, folder setup and server test (/health/check) endpoint

---
+++ server/app.ts
@@ -0,0 +1,31 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { type Express } from "express";
+
+// routes
+import authRoutes from "./routes/auth.routes.ts";
+import gameRoutes from "./routes/game.routes.ts";
+import healthRoutes from "./routes/health.routes.ts";
+import userRoutes from "./routes/user.routes.ts";
+
+const app: Express = express();
+const port = 3000;
+
+app.use(
+  cors({
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/health", healthRoutes);
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
+app.use("/game", gameRoutes);
+
+app.listen(port, () => {
+  console.log(`App is running at: http://localhost:${port}`);
+});
+
+export default app;
