fa158b4ea0d7deceb3bf093004db20b0fb7dcabe
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2026-08-17 14:27:18 +0530

    backend: comment out cookie settings in login route

--- server/routes/auth.routes.ts
+++ server/routes/auth.routes.ts
@@ -42,13 +42,13 @@ router.route("/login").post(async (req, res) => {
 
     const { token, publicUser } = await authService.loginUser(email, password);
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      partitioned: true,
-      maxAge: 24 * 60 * 60 * 1000,
-    });
+    // res.cookie("token", token, {
+    //   httpOnly: true,
+    //   secure: true,
+    //   sameSite: "none",
+    //   partitioned: true,
+    //   maxAge: 24 * 60 * 60 * 1000,
+    // });
 
     return res.status(200).json({
       message: "Login successful!",
